3ace690a58097b39f8e135ca6b28c303c960f93a
ThunderQuiz <147710821+ThunderQuiz@users.noreply.github.com>
2023-10-19 21:51:26 +0200

    Update dati.js

--- js/dati.js
+++ js/dati.js
@@ -1,4 +1,4 @@
-const poeti =[
+const poeti = [
     {
         nome: "Dante",
         tag: ["poeta", "stilnovo", "1300", "firenze", "politica"],
@@ -14,28 +14,63 @@ const poeti =[
         tag: ["poeta", "umanesimo", "1400", "firenze", "politica"],
         opere: ["Il Principe", "Epistolario", "La Mandragola"]
     }
-];
+]
 
-function randomIp(){
-    var netID = Math.floor(Math.random()*223);
-    return netID + "." + Math.floor(Math.random()*225) + "." + Math.floor(Math.random()*225) + "." + Math.floor(Math.random()*225);
+function operaLetteratura() {
+    var random = Math.floor(Math.random() * poeti.length);
+    var poeta = poeti[random];
+    random = Math.floor(Math.random() * poeta.opere.length);
+    var opera = poeta.opere[random];
+
+    return [opera, poeta.nome];
 }
-function ipClass(ip){
+
+function getPoeta(tag) {
+    var risultati = [];
+    if (tag == undefined) {
+        risultati = poeti;
+    }
+    else {
+        for (const persona of poeti) {
+            for (var line of tag) {
+                if (persona.tag.includes(line)) {
+                    risultati.push(persona.nome);
+                }
+            }
+
+        }
+    }
+    var random = generaValoriCasualiUnici(0, risultati.length - 1, 2);
+    var end = [];
+
+    for (i = 0; i < 2; i++) {
+        end.push(risultati[random[i]].nome);
+    }
+
+    console.log(end);
+    return end;
+}
+
+function randomIp() {
+    var netID = Math.floor(Math.random() * 223);
+    return netID + "." + Math.floor(Math.random() * 225) + "." + Math.floor(Math.random() * 225) + "." + Math.floor(Math.random() * 225);
+}
+function ipClass(ip) {
     var netID = ip.substr(0, 3);
-    if(parseInt(netID) < 128) return "A";
-    else if(parseInt(netID) < 192) return "B";
-    else if(parseInt(netID) < 224) return "C";
+    if (parseInt(netID) < 128) return "A";
+    else if (parseInt(netID) < 192) return "B";
+    else if (parseInt(netID) < 224) return "C";
     else return null;
 }
-function funz_matematica(){
+function funz_matematica() {
     var random = Math.floor(Math.random() * 5);
-    switch(random){
+    switch (random) {
         case 0:
             //funzione mista
             break;
 
         case 1:
-            //funzione fratta
+        //funzione fratta
 
         case 2:
             //funzione irrazionale
@@ -46,4 +81,18 @@ function funz_matematica(){
             break;
 
     }
+}
+
+function generaValoriCasualiUnici(min, max, quantità) {
+    if (max - min + 1 < quantità) {
+        throw new Error('Impossibile generare valori unici nel range specificato.');
+    }
+
+    const valoriUnici = new Set();
+    while (valoriUnici.size < quantità) {
+        const valoreCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
+        valoriUnici.add(valoreCasuale);
+    }
+
+    return Array.from(valoriUnici);
 }
